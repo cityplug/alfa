0fb10428bcbb355689f2fc674972d83876972f3c
cityplug <seni@cityplug.co.uk>
2024-01-04 17:07:09 +0000

    .

---
+++ wazuh/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Debian 16GB / 1 CORE - (wazuh.alfa.cityplug.io) setup script.
+
+# apt update && apt install git -y && cd /opt && git clone https://github.com/cityplug/alfa && apt full-upgrade -y && chmod +x /opt/alfa/wazuh/run.sh && reboot
+# mv /opt/alfa/wazuh/ /opt && rm -rf /opt/alfa/ && cd /opt/wazuh && ./run.sh
+
+# --- Install Packages
+apt update && apt full-upgrade -y
+apt install ca-certificates curl gnupg cifs-utils -y
+
+# --- Install Docker & Docker Compose 
+curl -sSL https://get.docker.com/ | sh && apt install docker-compose -y
+
+# --- Portainer Agent service
+docker run -d -p 9001:9001 --name portainer_agent --restart always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent:latest
+
+# --- Addons
+rm -rf /etc/update-motd.d/* && rm -rf /etc/motd
+mv /opt/alfa/wazuh/10-uname /etc/update-motd.d/ && chmod +x /etc/update-motd.d/10-uname
+
+#--
+systemctl enable docker 
+docker-compose --version && docker --version
+docker compose up -d
+docker ps
+#--------------------------------------------------------------------------------
