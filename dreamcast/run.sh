a97010aead4b36106530465fedbe3306918181d7
cityplug <seni@cityplug.co.uk>
2024-01-05 11:12:07 +0000

    .

--- dreamcast/run.sh
+++ dreamcast/run.sh
@@ -2,12 +2,8 @@
 
 # Debian 8GB - (dreamcast.alfa.cityplug.io) setup script.
 
-# apt update && apt install git -y && cd /opt && git clone https://github.com/cityplug/alfa && apt full-upgrade -y && chmod +x /opt/alfa/dreamcast/run.sh && reboot
-# mv /opt/alfa/dreamcast/ /opt && rm -rf /opt/alfa/ && cd /opt/dreamcast && ./run.sh
-
-# --- Install Packages
-apt update && apt full-upgrade -y
-apt install ca-certificates curl gnupg cifs-utils -y
+# cd /opt && apt update -y && apt install git curl gnupg cifs-utils -y && apt full-upgrade -y && git clone https://github.com/cityplug/alfa && reboot
+# chmod +x /opt/alfa/dreamcast/* && mv /opt/alfa/dreamcast/ /opt && rm -rf /opt/alfa/ && cd /opt/dreamcast && ./run.sh
 
 # --- Install Docker & Docker Compose 
 curl -sSL https://get.docker.com/ | sh && apt install docker-compose -y
@@ -17,7 +13,8 @@ docker run -d -p 9001:9001 --name portainer_agent --restart always -v /var/run/d
 
 # --- Addons
 rm -rf /etc/update-motd.d/* && rm -rf /etc/motd
-mv /opt/alfa/dreamcast/10-uname /etc/update-motd.d/ && chmod +x /etc/update-motd.d/10-uname
+wget https://raw.githubusercontent.com/cityplug/alfa/main/10-uname -O /etc/update-motd.d/10-uname && chmod +x /etc/update-motd.d/10-uname
+#mv /opt/alfa/dreamcast/10-uname /etc/update-motd.d/ && chmod +x /etc/update-motd.d/10-uname
 
 #--
 systemctl enable docker 
