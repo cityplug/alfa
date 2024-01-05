e9834e3a75eff0eff5701ef7635f32813acd122d
cityplug <seni@cityplug.co.uk>
2024-01-05 12:13:49 +0000

    .

--- dreamcast/run.sh
+++ dreamcast/run.sh
@@ -12,9 +12,8 @@ curl -sSL https://get.docker.com/ | sh && apt install docker-compose -y
 docker run -d -p 9001:9001 --name portainer_agent --restart always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent:latest
 
 # --- Addons
-rm -rf /etc/update-motd.d/* && rm -rf /etc/motd
+rm -rf /etc/update-motd.d/* && rm -rf /etc/motd && 
 wget https://raw.githubusercontent.com/cityplug/alfa/main/10-uname -O /etc/update-motd.d/10-uname && chmod +x /etc/update-motd.d/10-uname
-#mv /opt/alfa/dreamcast/10-uname /etc/update-motd.d/ && chmod +x /etc/update-motd.d/10-uname
 
 #--
 systemctl enable docker 
@@ -25,6 +24,6 @@ docker ps
 # --- Build Homepage
 docker stop homepage
 rm -rf /env/appdata/homepage/
-mv /opt/alfa/dreamcast/homepage /env/appdata/
+mv /opt/dreamcast/homepage /env/appdata/
 docker start homepage
 #--------------------------------------------------------------------------------
--- portainer/run.sh
+++ portainer/run.sh
@@ -2,8 +2,8 @@
 
 # Debian 8GB - (portainer.alfa.cityplug.io) setup script.
 
-# apt update && apt install git -y && cd /opt && git clone https://github.com/cityplug/alfa && apt full-upgrade -y && chmod +x /opt/alfa/portainer/run.sh && reboot
-# mv /opt/alfa/portainer/ /opt && rm -rf /opt/alfa/ && cd /opt/portainer && ./run.sh
+# cd /opt && apt update -y && apt install git curl gnupg cifs-utils -y && apt full-upgrade -y && git clone https://github.com/cityplug/alfa && reboot
+# chmod +x /opt/alfa/portainer/* && mv /opt/alfa/portainer/ /opt && rm -rf /opt/alfa/ && cd /opt/portainer && ./run.sh
 
 # --- Install Packages
 apt update && apt full-upgrade -y
@@ -12,8 +12,9 @@ apt install ca-certificates curl gnupg cifs-utils -y
 # --- Install Docker & Docker Compose 
 curl -sSL https://get.docker.com/ | sh && apt install docker-compose -y
 
-# --- Portainer Main service
+# --- Portainer & Uptime-kuma Services
 docker run -d -p 9000:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /env/appdata/portainer:/data portainer/portainer-ce:latest
+docker run -d -p 13001:3001 --name uptime-kuma --restart=always -v /env/appdata/uptime-kuma/data:/app/data elestio/uptime-kuma:latest
 
 # --- Addons
 rm -rf /etc/update-motd.d/* && rm -rf /etc/motd
